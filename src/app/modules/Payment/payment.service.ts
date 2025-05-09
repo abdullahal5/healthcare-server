954630384d5cda7250e1ffbf6ca159706a10fa9d
abdullahal5 <abdullahalfahin183@gmail.com>
2025-05-09 14:52:33 +0600

    ssl commerz install

---
+++ src/app/modules/Payment/payment.service.ts
@@ -0,0 +1,12 @@
+const initPayment = async (appointmentId: string) => {
+
+};
+
+const validatePayment = async (payload: any) => {
+
+};
+
+export const PaymentService = {
+  initPayment,
+  validatePayment,
+};
