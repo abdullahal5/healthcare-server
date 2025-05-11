e6308a8e42acc1f53c0c852f8a17ff4125eeeb01
abdullahal5 <abdullahalfahin183@gmail.com>
2025-05-11 13:48:55 +0600

    average rating updated

--- src/app/modules/Review/review.service.ts
+++ src/app/modules/Review/review.service.ts
@@ -2,10 +2,110 @@ import httpStatus from "http-status";
 import { Prisma } from "@prisma/client";
 import { JwtPayload } from "jsonwebtoken";
 import { IPaginations } from "../../interfaces/pagination";
+import { prisma } from "../../../shared/prisma";
+import AppError from "../../../shared/appError";
+import { calculatePagination } from "../../../helper/paginationHelper";
 
-const insertIntoDB = async (user: JwtPayload, payload: any) => {};
+const insertIntoDB = async (user: JwtPayload, payload: any) => {
+  const patientData = await prisma.patient.findFirstOrThrow({
+    where: {
+      email: user.email,
+    },
+  });
 
-const getAllFromDB = async (filters: any, options: IPaginations) => {};
+  const appointmentData = await prisma.appointment.findUniqueOrThrow({
+    where: {
+      id: payload.appointmentId,
+    },
+  });
+
+  if (patientData.id !== appointmentData.patientId) {
+    throw new AppError(httpStatus.BAD_REQUEST, "This is not your appointment");
+  }
+
+  return await prisma.$transaction(async (tx) => {
+    const result = await prisma.review.create({
+      data: {
+        appointmentId: appointmentData.id,
+        doctorId: appointmentData.doctorId,
+        patientId: appointmentData.patientId,
+        rating: payload.rating,
+        comment: payload.comment,
+      },
+    });
+
+    const avgRating = await tx.review.aggregate({
+      _avg: {
+        rating: true,
+      },
+    });
+
+    await tx.doctor.update({
+      where: {
+        id: result.doctorId,
+      },
+      data: {
+        averageRating: avgRating._avg.rating as number,
+      },
+    });
+
+    return result;
+  });
+};
+
+const getAllFromDB = async (filters: any, options: IPaginations) => {
+  const { limit, page, skip } = calculatePagination(options);
+  const { patientEmail, doctorEmail } = filters;
+  const andConditions = [];
+
+  if (patientEmail) {
+    andConditions.push({
+      patient: {
+        email: patientEmail,
+      },
+    });
+  }
+
+  if (doctorEmail) {
+    andConditions.push({
+      doctor: {
+        email: doctorEmail,
+      },
+    });
+  }
+
+  const whereConditions: Prisma.ReviewWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.review.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? { [options.sortBy]: options.sortOrder }
+        : {
+            createdAt: "desc",
+          },
+    include: {
+      doctor: true,
+      patient: true,
+      appointment: true,
+    },
+  });
+  const total = await prisma.review.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
 
 export const ReviewService = {
   insertIntoDB,
